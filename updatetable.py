24dbae2d2b96da48fbdcf36182ca0d24d2f5a224
tzutalin <tzu.ta.lin@gmail.com>
2016-06-25 23:50:37 +0800

    add window/mac ndk link

--- updatetable.py
+++ updatetable.py
@@ -14,25 +14,26 @@ def appendToTable(title, link):
     jsonFile='table.json'
     with open(jsonFile) as json_file:
         json_data = json.load(json_file)
-        print(json_data)
 
     if json_data is not None:
+        print "Append a new ndk download_link if not existed:" + title
         json_data[title] = link
         with open(jsonFile, mode='w') as f:
-            json.dump(json_data, f)
+            json.dump(json_data, f, indent=4, sort_keys=True)
 
 if __name__=='__main__':
     print 'Getting the latest NDK version'
     soup = getSoup('https://developer.android.com/ndk/downloads/index.html')
     all_td = soup.findAll('td')
 
-    linux_td = None
-    for each_td in all_td:
-        if 'linux' in str(each_td):
-            linux_td = each_td
-            break
+    download_os = ['linux', 'windows', 'darwin']
+    for os_td in download_os:
+        for each_td in all_td:
+            if os_td in str(each_td):
+                os_td = each_td
+                break
 
-    download_link=linux_td.a.get('href')
-    title=linux_td.a.getText().split('.', 1)[0]
-    appendToTable(title, download_link)
+        download_link=os_td.a.get('href')
+        title=os_td.a.getText().split('.', 1)[0]
+        appendToTable(title, download_link)
 
